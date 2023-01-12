cfc0dd535e028799013de45feabcc322684fe83d
Corey Cole <coreyleoc@gmail.com>
2023-01-12 01:03:56 -0800

    fix(lsp): go to definition keymap

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -26,9 +26,9 @@ local on_attach = function(client, bufnr)
     local opts = {noremap = true, silent = true}
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    -- buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
     -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    -- buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
     -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
     vim.api.nvim_create_autocmd("CursorHold", {
         buffer = bufnr,
