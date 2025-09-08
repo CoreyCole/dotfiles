6250cb3d4185a0c8430b7e8b3d0d71b8ea14523d
Corey Cole <coreyleoc@gmail.com>
2025-09-07 22:10:32 -0700

    wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,89 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local on_windows = wezterm.target_triple:find("windows")
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+if on_windows then
+	config.default_prog = { "powershell.exe" }
+end
+
+config.font = wezterm.font("Hasklug Nerd Font Mono Med")
+config.audible_bell = "Disabled"
+config.scrollback_lines = 10000
+local disable = "DisableDefaultAssignment"
+
+-- Disable all default mouse bindings so only ours apply
+config.mouse_bindings = {
+	-- Normal click: just select text
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("PrimarySelection"),
+	},
+
+	-- Shift+click: open link
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "SHIFT",
+		action = act.OpenLinkAtMouseCursor,
+	},
+
+	-- Right click: paste from clipboard
+	{
+		event = { Up = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = act.PasteFrom("Clipboard"),
+	},
+}
+
+config.keys = {
+	-- Alt+1 through Alt+9 for tab switching
+	{ key = "1", mods = "ALT", action = act.ActivateTab(0) },
+	{ key = "2", mods = "ALT", action = act.ActivateTab(1) },
+	{ key = "3", mods = "ALT", action = act.ActivateTab(2) },
+	{ key = "4", mods = "ALT", action = act.ActivateTab(3) },
+	{ key = "5", mods = "ALT", action = act.ActivateTab(4) },
+	{ key = "6", mods = "ALT", action = act.ActivateTab(5) },
+	{ key = "7", mods = "ALT", action = act.ActivateTab(6) },
+	{ key = "8", mods = "ALT", action = act.ActivateTab(7) },
+	{ key = "9", mods = "ALT", action = act.ActivateTab(8) },
+	{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
+	{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
+	{ key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
+	{ key = "w", mods = "ALT", action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = "x", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
+	{ key = "X", mods = "CTRL", action = act.ActivateCopyMode },
+	{ key = "X", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
+	{ key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
+	{ key = "C", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
+	{ key = "c", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
+	{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+	{ key = "Copy", mods = "NONE", action = act.CopyTo("Clipboard") },
+	{ key = "Paste", mods = "NONE", action = act.PasteFrom("Clipboard") },
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "V", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "v", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+	{ key = "F", mods = "CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
+	{ key = "F", mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
+	{ key = "N", mods = "CTRL", action = act.SpawnWindow },
+	{ key = "N", mods = "SHIFT|CTRL", action = act.SpawnWindow },
+	{ key = "n", mods = "SHIFT|CTRL", action = act.SpawnWindow },
+	{ key = "n", mods = "SUPER", action = act.SpawnWindow },
+	{ key = "P", mods = "CTRL", action = act.ActivateCommandPalette },
+	{ key = "P", mods = "SHIFT|CTRL", action = act.ActivateCommandPalette },
+	{ key = "p", mods = "SHIFT|CTRL", action = act.ActivateCommandPalette },
+	{ key = "R", mods = "CTRL", action = act.ReloadConfiguration },
+	{ key = "R", mods = "SHIFT|CTRL", action = act.ReloadConfiguration },
+	{ key = "T", mods = "CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "T", mods = "SHIFT|CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "t", mods = "SHIFT|CTRL", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "t", mods = "SUPER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "f", mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
+	{ key = "f", mods = "SUPER", action = act.Search("CurrentSelectionOrEmptyString") },
+}
+
+return config
