8230abcd512f55f51357b5eec64cf59a67338b29
Corey Cole <coreyleoc@gmail.com>
2025-09-07 22:20:21 -0700

    clean up finicky

--- finicky.js
+++ finicky.js
@@ -1,16 +1,6 @@
 // ~/.finicky.js
 export default {
   defaultBrowser: "Zen Browser",
-  rewrite: [
-    {
-      // Open Linear links in the native app
-      match: "https://linear.app/*",
-      url: (url) => {
-        // Convert https://linear.app/... to linear://linear.app/...
-        return url.toString().replace("https://", "file://linear://");
-      },
-    },
-  ],
   handlers: [
     {
       // Open Google Meet (Hangouts) links in Arc
