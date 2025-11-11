574eeb17f3198d0b0e2d6340aa05cd0245ec4e31
Maria José Solano <majosolano99@gmail.com>
2025-11-11 12:44:39 -0800

    setup Lua debugger

--- neovim-config/lua/plugins/init.lua
+++ neovim-config/lua/plugins/init.lua
@@ -603,6 +603,25 @@ return {
     {
         "mfussenegger/nvim-dap",
         event = "VeryLazy",
+        dependencies = {
+            {
+                "theHamsta/nvim-dap-virtual-text",
+                opts = {},
+            },
+            -- Lua adapter.
+            {
+                "jbyuki/one-small-step-for-vimkind",
+                keys = {
+                    {
+                        "<leader>dl",
+                        function()
+                            require("osv").launch { port = 8086 }
+                        end,
+                        desc = "Launch Lua adapter",
+                    },
+                },
+            },
+        },
         keys = {
             {
                 "<leader>db",
@@ -805,6 +824,18 @@ return {
             dap.listeners.before.event_exited["dapui_config"] = function()
                 dapui.close {}
             end
+
+            -- Lua configurations.
+            dap.adapters.nlua = function(callback, config)
+                callback { type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 }
+            end
+            dap.configurations["lua"] = {
+                {
+                    type = "nlua",
+                    request = "attach",
+                    name = "Attach to running Neovim instance",
+                },
+            }
         end,
         keys = {
             {
@@ -822,10 +853,6 @@ return {
                 desc = "[d]ap [e]val",
             },
         },
-    },
-    {
-        "theHamsta/nvim-dap-virtual-text",
-        opts = {},
     },
     -- {
     --     "mfussenegger/nvim-dap-python",
